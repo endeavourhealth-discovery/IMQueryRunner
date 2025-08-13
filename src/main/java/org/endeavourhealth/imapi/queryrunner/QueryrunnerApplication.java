56a9762f2c87b646cb937ea17c6136506995781e
matt-voror <matt@voror.co.uk>
2025-08-13 15:37:32 +0100

    initialise java/spring/gradle

---
+++ src/main/java/org/endeavourhealth/imapi/queryrunner/QueryrunnerApplication.java
@@ -0,0 +1,13 @@
+package org.endeavourhealth.imapi.queryrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryrunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryrunnerApplication.class, args);
+	}
+
+}
