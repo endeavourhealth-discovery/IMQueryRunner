970c013d5240d12b5b3caddeac1d13d9d8742e3a
Richard Collier <richard.collier@voror.co.uk>
2025-09-23 13:58:41 +0100

    refactor(api): enhance query results with caching and add status endpoint
    Added MySQL cache lookup using object-hash for query requests
    and created new status endpoint for queue items.

---
+++ server/api/queue/query/results/[queueId].get.ts
@@ -0,0 +1,29 @@
+import { QueueItemStatus } from "~~/enums";
+import { z } from "zod";
+import {postgresDb} from "~~/server/db/postgres";
+import {eq, sql} from "drizzle-orm";
+import {queueItem} from "~~/server/db/postgres/schema";
+import hash from "object-hash";
+import {mysqlDb} from "~~/server/db/mysql";
+
+const paramSchema = z.object({
+  queueId: z.string(),
+});
+
+export default defineEventHandler(async (event) => {
+  const { queueId } = await getValidatedRouterParams(event, paramSchema.parse);
+  const item = await postgresDb.query.queueItem.findFirst({
+    where: eq(queueItem.id, queueId ),
+  });
+
+  if (item?.queryRequest) {
+    const requestHash = hash(item.queryRequest);
+    console.log(requestHash);
+
+    const results = await mysqlDb.execute(
+      sql.raw(`SELECT * FROM imqcache.${requestHash}`)
+    );
+
+    return results[0];
+  }
+});
