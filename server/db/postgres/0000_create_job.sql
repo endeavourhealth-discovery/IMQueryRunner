7966e61385e5c8e673cae80fba3211ded1c9c9f2
kkardatos <kon.kardatos@gmail.com>
2026-02-10 09:24:50 +0000

    update create job table script

--- server/db/postgres/0000_create_job.sql
+++ server/db/postgres/0000_create_job.sql
@@ -1,17 +1,14 @@
 -- CREATE SCHEMA query_runner; -- uncomment if schema does not exist
-
 CREATE TABLE
   query_runner.job (
-    id UUID PRIMARY KEY, -- Unique run identifier
+    dbid UUID PRIMARY KEY, -- Unique run identifier
     job_name VARCHAR(255) NOT NULL,
-    query_iri VARCHAR(255) NOT NULL,
     query_request JSONB NOT NULL,
     user_id UUID NOT NULL,
-    user_name VARCHAR(255) NOT NULL,
-    queued_at TIMESTAMP NOT NULL,
-    started_at TIMESTAMP,
-    pid INT, -- Internal (postgres) process ID (for killing)
-    stopped_at TIMESTAMP,
-    status TEXT,
+    queue_date TIMESTAMP NOT NULL,
+    run_date TIMESTAMP,
+    finish_date TIMESTAMP,
+    pid INT, -- Internal (mysql) process ID (for killing)
+    status TEXT NOT NULL,
     error TEXT
   );
