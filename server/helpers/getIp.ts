b971b5a945a5d2010c01d433888f38b2d1cc795e
matt-voror <matt@voror.co.uk>
2026-02-18 10:08:35 +0000

    remove comment

--- server/helpers/getIp.ts
+++ server/helpers/getIp.ts
@@ -4,7 +4,6 @@ import { normaliseIp } from "./normaliseIp";
 export function getIp(event: H3Event) {
   const ip = getRequestIP(event, { xForwardedFor: true });
   if (!ip) {
-    // if (import.meta.dev) return "127.0.0.1";
     throw createError({
       statusCode: 400,
       statusMessage: "IP address not available",
