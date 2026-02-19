7c2704c59533869d43ea03c6146c708b1b08832d
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2026-02-19 11:00:40 +0000

    removed download method

--- app/components/queryRunner/QueryResults.vue
+++ app/components/queryRunner/QueryResults.vue
@@ -2,61 +2,61 @@
   <div class="flex-auto overflow-auto">
     <div class="h-[calc(100% - 3.5rem)] overflow-auto">
       <div
-        class="flex h-full flex-auto flex-col flex-nowrap overflow-auto bg-(--p-content-background)"
+          class="flex h-full flex-auto flex-col flex-nowrap overflow-auto bg-(--p-content-background)"
       >
         <div class="m-2">
           <Button
-            icon="fa-solid fa-arrow-left"
-            label="Back to queue"
-            @click="backToQueue"
+              icon="fa-solid fa-arrow-left"
+              label="Back to queue"
+              @click="backToQueue"
           />
         </div>
         <DataTable
-          :size="'small'"
-          :value="queryResults"
-          :paginator="true"
-          :rows="rows"
-          :scrollable="true"
-          scroll-height="600px"
-          :autoLayout="true"
-          @page="onPage($event)"
-          :lazy="true"
-          :total-records="totalCount"
-          :rows-per-page-options="[
+            :size="'small'"
+            :value="queryResults"
+            :paginator="true"
+            :rows="rows"
+            :scrollable="true"
+            scroll-height="600px"
+            :autoLayout="true"
+            @page="onPage($event)"
+            :lazy="true"
+            :total-records="totalCount"
+            :rows-per-page-options="[
             originalSize,
             originalSize * 2,
             originalSize * 4,
             originalSize * 8,
           ]"
-          :loading="loading"
-          :paginatorTemplate="'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown'"
+            :loading="loading"
+            :paginatorTemplate="'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown'"
         >
           <template #header>
             <div class="flex flex-wrap items-center justify-between gap-2">
               <span class="text-xl font-bold"
-                >Total results: {{ totalCount }}</span
+              >Total results: {{ totalCount }}</span
               >
             </div>
           </template>
           <template #empty>None</template>
           <Column
-            v-for="col of columns"
-            :key="col.field"
-            :field="col.field"
-            :header="col.header"
+              v-for="col of columns"
+              :key="col.field"
+              :field="col.field"
+              :header="col.header"
           ></Column>
         </DataTable>
       </div>
       <div class="im-dialog-footer">
         <div class="button-footer">
           <Button
-            class="m-2"
-            :disabled="!queryResults.length"
-            data-testid="query-results-download"
-            label="Download"
-            :loading="downloadLoading"
-            @click="downloadQueryResults()"
-            autofocus
+              class="m-2"
+              :disabled="!queryResults.length"
+              data-testid="query-results-download"
+              label="Download"
+              :loading="downloadLoading"
+              @click="downloadQueryResults()"
+              autofocus
           />
         </div>
       </div>
@@ -65,17 +65,17 @@
 </template>
 
 <script setup lang="ts">
-import { isArray } from "lodash-es";
-import { onMounted, ref } from "vue";
-import type { Ref } from "vue";
-import { useUserStore } from "~/stores/useUserStore";
+import {isArray} from "lodash-es";
+import {onMounted, ref} from "vue";
+import type {Ref} from "vue";
+import {useUserStore} from "~/stores/useUserStore";
 
 interface Props {
   jobId: string | string[] | undefined;
 }
 
 const props = defineProps<Props>();
-const { user } = useUserStore();
+const {user} = useUserStore();
 
 const loading = ref(false);
 const downloadLoading = ref(false);
@@ -94,14 +94,14 @@ onMounted(async () => {
 async function getQueryResults() {
   if (props.jobId) {
     const value = await $fetch<{ totalCount: number; result: any[] }>(
-      `/api/queue/job/results/${props.jobId}`,
-      {
-        query: {
-          userId: user?.id,
-          page: page.value,
-          size: rows.value,
+        `/api/queue/job/results/${props.jobId}`,
+        {
+          query: {
+            userId: user?.id,
+            page: page.value,
+            size: rows.value,
+          },
         },
-      },
     );
     if (value && isArray(value.result)) {
       totalCount.value = value.totalCount;
@@ -113,36 +113,11 @@ async function getQueryResults() {
 function formatResultsForTable() {
   for (const key of Object.keys(queryResults.value[0])) {
     if (key !== "hashcode")
-      columns.value.push({ field: key, header: key.replace("_", " ") });
+      columns.value.push({field: key, header: key.replace("_", " ")});
   }
 }
 
 function downloadQueryResults() {
-  const headers = Object.keys(queryResults.value[0]);
-
-  const csv = [
-    headers.join(","),
-    ...queryResults.value.map(row =>
-        headers
-            .map(field => {
-              const value = row[field] ?? "";
-              return `"${String(value).replace(/"/g, '""')}"`;
-            })
-            .join(",")
-    )
-  ].join("\n");
-
-  const blob = new Blob([csv], {type: "text/csv;charset=utf-8;"});
-  const url = URL.createObjectURL(blob);
-
-  const link = document.createElement("a");
-  link.href = url;
-  link.setAttribute("download", "filename");
-  document.body.appendChild(link);
-  link.click();
-
-  document.body.removeChild(link);
-  URL.revokeObjectURL(url);
 }
 
 async function onPage(event: any) {
