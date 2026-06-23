6ef5cffea3b9bf0ed33f1ec5b71f480d3ca956f3
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2026-06-23 11:04:11 +0100

    pre-commit script for library link: and file: checks

---
+++ scripts/commit-link-check.cjs
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const files = [
+  'pnpm-workspace.yaml',
+  'pnpm-lock.yaml',
+];
+
+const violations = [];
+
+const patterns = [
+  'link:',
+  'file:',
+];
+
+for (const file of files) {
+  if (!fs.existsSync(file)) continue;
+
+  const content = fs.readFileSync(file, 'utf8');
+  const matches = content.match(/(link:)/);
+
+  if (matches?.length) {
+    violations.push({ file, matches });
+  }
+}
+
+if (violations.length) {
+  console.error('\nLocal VueLibrary must be unlinked before committing. Please use \'pnpm unlink\' and/or remove all link: and file: references from:\n');
+  for (const f of files) console.error(' - ' + f);
+  console.error('');
+  process.exit(1);
+}
+process.exit(0);
