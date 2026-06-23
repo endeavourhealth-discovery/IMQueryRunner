dea00ad42d96b818a9d9b708bd7b63bc135b3c21
Jo Stiansen <Jo.Stiansen@voror.co.uk>
2026-06-23 15:38:27 +0100

    fixed commit check script to include file:

--- scripts/commit-link-check.cjs
+++ scripts/commit-link-check.cjs
@@ -7,23 +7,16 @@ const files = [
   'pnpm-lock.yaml',
 ];
 
-const violations = [];
+let matches = [];
 
 for (const file of files) {
   if (!fs.existsSync(file)) continue;
-
   const content = fs.readFileSync(file, 'utf8');
-  const matches = content.match(/'@endeavour\/vue-library':\s*(?:file|link):/);
-
-  if (matches?.length) {
-    violations.push({ file, matches });
-  }
+  matches = content.match(/'@endeavour\/vue-library':\s*(?:file|link):/);
 }
 
-if (violations.length) {
-  console.error('\nLocal VueLibrary must be unlinked before committing. ' +
-    'Please use \'pnpm unlink\' and/or make sure link: or file: overrides ' +
-    'are removed from pnpm-workspace.yaml before running pnpm install.');
+if (matches?.length) {
+  console.error('\nLocal VueLibrary must be unlinked before committing.\nPlease use \'pnpm unlink\' and/or make sure link: or file: overrides are removed from pnpm-workspace.yaml before running pnpm install.\n');
   process.exit(1);
 }
 process.exit(0);
